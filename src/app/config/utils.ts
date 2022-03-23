99f09e16870e6c940b3e32cf8dcc5339a5bc2c38
Arsen Tamazyan <atamazyan@tsconsulting.com>
2022-03-23 15:12:24 +0300

    добавлен utils file

---
+++ src/app/config/utils.ts
@@ -0,0 +1,7 @@
+export function copyObject<T>(obj: Record<string, unknown>): T {
+  return JSON.parse(JSON.stringify(obj)) as T;
+}
+
+export function log(...args: unknown[]) {
+  console.log(JSON.parse(JSON.stringify(args)));
+}
